9847507f26ee4317db894f6b8572e60dbb0b3b3a
ilyareshetov <ilyareshetov@outlook.com>
2017-02-19 12:49:21 +0300

    first

--- UnitTest/Form1.cs
+++ UnitTest/Form1.cs
@@ -16,5 +16,11 @@ namespace UnitTest
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StringFormatter obj = new StringFormatter();
+            textBox2.Text = obj.WebString(textBox1.Text);
+        }
     }
 }
---
+++ UnitTest/StringFormatter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    class StringFormatter
+    {
+        public string WebString(string s)
+        {
+            string str = "", http = "http://", https = "https://", sex = "secure";
+            if (!s.Contains(http) && !s.Contains(sex)) {
+                str = http + s;
+            }
+            else if (s.Contains(sex)) {
+                str = https + s;
+            }
+
+            return str;
+        }
+    }
+}
