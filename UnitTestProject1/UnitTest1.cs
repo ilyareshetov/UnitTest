f5619b16c1efc7334409ac1beadbee56033e3462
Евгений <@MSI>
2017-02-19 19:56:08 +0300

    five tests

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -7,14 +7,46 @@ namespace UnitTestProject1
     [TestClass]
     public class UnitTest1
     {
+        StringFormatter _setstring = new StringFormatter();
+
         [TestMethod]
         public void TestWebStrings()
         {
-            StringFormatter _setstring = new StringFormatter();
-
             string res = _setstring.WebString("google.ru");
 
             Assert.AreEqual(res, "http://google.ru");
+        }
+
+        [TestMethod]
+        public void TestSexStrings()
+        {
+            string res = _setstring.WebString("secure.google.ru");
+
+            Assert.AreEqual(res, "https://secure.google.ru");
+        }
+
+        [TestMethod]
+        public void TestNullStrings()
+        {
+            string res = _setstring.WebString("");
+
+            Assert.AreEqual(res, "ErrorNullString");
+        }
+
+        [TestMethod]
+        public void TestHttpStrings()
+        {
+            string res = _setstring.WebString("http://google.ru");
+
+            Assert.AreEqual(res, "http://google.ru");
+        }
+
+        [TestMethod]
+        public void TestHttpsStrings()
+        {
+            string res = _setstring.WebString("https://secure.google.ru");
+
+            Assert.AreEqual(res, "https://secure.google.ru");
         }
     }
 }
